ca591b1c290038db3f0574c63ac75a0508c070df
Karasik953 <itprogergoit@gmail.com>
2025-10-15 21:31:27 +0300

    react

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient = new QueryClient();
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <App />
     <ReactQueryDevtools initialIsOpen={false} />
